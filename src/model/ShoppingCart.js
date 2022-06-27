af2f180b7fefdeaaf0a8752645e0d051639adb87
Liew Shi Hui <e0324820@u.nus.edu>
2022-06-27 13:07:13 +0800

    Implement new task
    Implement a discount of 20%, the products eligible for which will be identified by the product code starting with "DIS_20". On purchase of each such product, the customer earns, 1 loyalty point for every $20 spent

--- src/model/ShoppingCart.js
+++ src/model/ShoppingCart.js
@@ -2,43 +2,65 @@ import _ from "lodash";
 import Order from "./Order.js";
 
 export default class ShoppingCart {
-    constructor(customer, products) {
-        this.customer = customer;
-        this.products = products;
-    };
+  constructor(customer, products) {
+    this.customer = customer;
+    this.products = products;
+  }
 
-    addProduct = (product) => {
-        this.products.push(product);
-    };
+  addProduct = (product) => {
+    this.products.push(product);
+  };
 
-    removeProduct = (product) => {
-        _.remove(this.products, product);
-    };
+  removeProduct = (product) => {
+    _.remove(this.products, product);
+  };
 
-    checkout = () => {
-        let totalPrice = 0;
-        let loyaltyPointsEarned = 0;
+  checkout = () => {
+    let totalPrice = 0;
+    let loyaltyPointsEarned = 0;
 
-        this.products.forEach(product => {
-            let discount = 0;
-            if (product.code.startsWith("DIS_10")) {
-                discount = product.price * 0.1;
-                loyaltyPointsEarned += product.price / 10;
-            } else if (product.code.startsWith("DIS_15")) {
-                discount = product.price * 0.15;
-                loyaltyPointsEarned += product.price / 15;
-            } else {
-                loyaltyPointsEarned += product.price / 5;
-            }
+    this.products.forEach((product) => {
+      let { discount, loyaltyPointsEarned: lp } =
+        this.calcDiscountAndLoyaltyPoints(product.code, product.price);
 
-            totalPrice += product.price - discount;
-        });
+      loyaltyPointsEarned += lp;
 
-        return new Order(totalPrice, loyaltyPointsEarned);
-    };
+      totalPrice += product.price - discount;
+    });
 
-    displaySummary = () =>  {
-        return "Customer: " + this.customer.name + "\n" + 
-            "Bought:  \n" + this.products.map(p => "- " + p.name + ", " + p.price).join('\n');
+    return new Order(totalPrice, loyaltyPointsEarned);
+  };
+
+  calcDiscountAndLoyaltyPoints = (productCode, price) => {
+    if (productCode.startsWith("DIS")) {
+      let productCodeArr = productCode.split("_");
+      let discount = productCodeArr[1];
+
+      discount = parseInt(discount); // 10, 15, 20
+      let discountProp = discount / 100; // 0.1, 0.15, 0.2
+
+      let discounted = price * discountProp;
+      let loyaltyPointsEarned = price / discount;
+
+      return {
+        discount: discounted,
+        loyaltyPointsEarned: loyaltyPointsEarned,
+      };
+    } else {
+      return {
+        discount: 0,
+        loyaltyPointsEarned: price / 5,
+      };
     }
-};
+  };
+
+  displaySummary = () => {
+    return (
+      "Customer: " +
+      this.customer.name +
+      "\n" +
+      "Bought:  \n" +
+      this.products.map((p) => "- " + p.name + ", " + p.price).join("\n")
+    );
+  };
+}
